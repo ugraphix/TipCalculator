ab5556038026a302aa03c078fd6a036476cd4c71
Gracie <grace@ugraphix.com>
2017-01-18 10:57:02 -0800

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+<a href="Default.aspx.cs">Default.aspx.cs</a>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>Tip Calculator</h1>
+    <div>
+        <p></p>
+        <asp:Label ID="MealAmount" runat="server" Text="Enter Meal Amount"></asp:Label>
+        <!--renamed because Conger will refer to the textbox in code-->
+        <asp:TextBox ID="MealTextBox" runat="server"></asp:TextBox>
+        <asp:RadioButtonList ID="TipPercentRadios" runat="server"></asp:RadioButtonList>
+        <asp:TextBox ID="OtherTextbox" runat="server"></asp:TextBox>
+        <br><asp:Button ID="SubmitButton" runat="server" Text="Submit" OnClick="SubmitButton_Click" /></br>
+        <asp:Label ID="Results" runat="server" Text=""></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack) //holds the data in the form before hitting submit. Selecting 15% and then submitting doesnt clear the radio button selection
+        {
+            string[] tipPercent = { "10%", "15%", "20%", "Other" };
+            TipPercentRadios.DataSource = tipPercent;
+            TipPercentRadios.DataBind();
+        }
+    }
+
+    protected void SubmitButton_Click(object sender, EventArgs e)
+    {
+        GetInfo();
+    }
+
+    protected void GetInfo()
+    {
+        Tip tip = new Tip();
+        tip.MealAmount = double.Parse(MealTextBox.Text);  //parse looks at text from textbox and thinks "can i convert this to double?")
+        if (OtherTextbox.Text == "")
+        {
+            tip.TipPercent = 0;
+            foreach (ListItem item in TipPercentRadios.Items)//looping through all radio list buttons, rmemeber that ALL TEXT MUST MATCH
+            {
+
+                if (item.Selected)
+                {
+
+                    if (item.Text.Equals("10%"))
+                    {
+                        tip.TipPercent = .1;
+                    }
+                    else if (item.Text.Equals("15%"))
+                    {
+                        tip.TipPercent = .15;
+                    }
+                    else if (item.Text.Equals("20%"))
+                    {
+                        tip.TipPercent = .20;
+                    }
+
+
+                }//end if selector
+            }//end for each tip
+        }//end outer if
+        else
+        {
+            tip.TipPercent = double.Parse(OtherTextbox.Text);
+        }
+        Results.Text = "Amount:" + tip.MealAmount.ToString("$#,# #0.00") + "<br/>" +
+             "Tax" + tip.CalculateTax().ToString(" $#,# #0.00") + "<br/>" +
+             "Tip" + tip.CalculateTip().ToString(" $#,# #0.00") + "<br/>" +
+             "Total" + tip.CalculateTotal().ToString(" $#,# #0.00");
+    }//end of getinfo Tip
+}
+
